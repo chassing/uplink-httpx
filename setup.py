4d44db9a51988637530aa1a58322828bdc4db821
Christian Assing <chris@ca-net.org>
2020-04-02 08:51:48 +0200

    Bump version: 1.1 → 1.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="uplink-httpx",
-    version="1.1",
+    version="1.2",
     description="HttpX Client for Uplink",
     long_description=Path("README.md").read_text(),
     long_description_content_type="text/markdown",
